91a5f49fad86802330d5d22ce9dfa773a52f87ea
Mourex99 <mateusmoura.st@outlook.com>
2023-05-20 01:45:44 -0300

    códigos comentados

--- teste.py
+++ teste.py
@@ -1,31 +1,31 @@
-name = (input("Qual o seu primeiro nome: "))
-print ("Olá,",name)
+# name = (input("Qual o seu primeiro nome: "))
+# print ("Olá,",name)
 
-nome = (input("Qual o seu nome: "))
-sobrenome = (input("Qual o seu sobrenome: "))
-print (nome, sobrenome)
+# nome = (input("Qual o seu nome: "))
+# sobrenome = (input("Qual o seu sobrenome: "))
+# print (nome, sobrenome)
 
-num1 = float (input("Escreva um números: "))
-num2 = float (input("EScreva o segundo número: "))
+# num1 = float (input("Escreva um números: "))
+# num2 = float (input("EScreva o segundo número: "))
 
-print (num1 + num2)
+# print (num1 + num2)
 
-num1 = int (input("Escreva o primeiro número: "))
-num2 = int (input("Escreva o segundo número: "))
-num3 = int (input("Escreva o terceiro número: "))
-calculo = (num1 + num2)*num3
+# num1 = int (input("Escreva o primeiro número: "))
+# num2 = int (input("Escreva o segundo número: "))
+# num3 = int (input("Escreva o terceiro número: "))
+# calculo = (num1 + num2)*num3
 
-print("O Valor é: ", calculo)
+# print("O Valor é: ", calculo)
 
-pizza = int (input("Quantos pedaços tem a pizza: "))
-comeu = int (input("Quantos pedaços você comeu: "))
-resto = int (pizza - comeu)
+# pizza = int (input("Quantos pedaços tem a pizza: "))
+# comeu = int (input("Quantos pedaços você comeu: "))
+# resto = int (pizza - comeu)
 
-print (f"sobrou {resto} pedaços no total.")
+# print (f"sobrou {resto} pedaços no total.")
 
-nome = (input("Qual o seu nome: "))
-idade = int (input("Qual a sua idade: "))
-somaIdade = int (idade + 1)
+# nome = (input("Qual o seu nome: "))
+# idade = int (input("Qual a sua idade: "))
+# somaIdade = int (idade + 1)
 
-print ("Seu nome: ", nome)
-print ("Sua idade no próximo ano será: ", somaIdade)
+# print ("Seu nome: ", nome)
+# print ("Sua idade no próximo ano será: ", somaIdade)
